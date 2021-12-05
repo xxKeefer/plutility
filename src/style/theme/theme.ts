af59bb869d9b0ffc75be88c0ddac56ac71f89594
xxKeefer <xxkeefer.code@gmail.com>
2021-12-05 10:00:21 +1000

    added theme

---
+++ src/style/theme/theme.ts
@@ -0,0 +1,24 @@
+const colors = {
+    white: '#fff',
+    black: '#000',
+    grey: '#dfdfdf',
+    red: '#fb1b1b0',
+    navy: '#0a285f',
+    blue: '#0075be',
+    yellow: '#ffcc00',
+    gold: '#d5a100',
+};
+
+const screenSizes = {
+    'screen-xs': '420px',
+    'screen-sm': '576px',
+    'screen-md': '768px',
+    'screen-lg': '992px',
+    'screen-xl': '1200px',
+    'screen-xxl': '1600px',
+};
+
+export default {
+    ...colors,
+    ...screenSizes,
+};
