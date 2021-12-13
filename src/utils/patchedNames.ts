fadc9ed2ce0fa3428d4cec4f35830a4d5b0fa097
xxKeefer <xxkeefer.code@gmail.com>
2021-12-14 07:45:52 +1000

    lycanroc patch

--- src/utils/patchedNames.ts
+++ src/utils/patchedNames.ts
@@ -1,12 +1,15 @@
 import pokemon from 'pokemon'
 
-const patchList: readonly string[] = ['Indeedee', 'Aegislash'] as const
+const patchList: readonly string[] = ['Indeedee', 'Aegislash', 'Lycanroc'] as const
 
 const patchedNames: readonly string[] = [
     'Indeedee-male',
     'Indeedee-female',
     'Aegislash-blade',
     'Aegislash-shield',
+    'Lycanroc-midday',
+    'Lycanroc-midnight',
+    'Lycanroc-dusk',
 ] as const
 
 const patch = (poke: string) => !patchList.includes(poke)
