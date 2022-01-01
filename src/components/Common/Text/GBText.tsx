ae961250001d66192298021ceaa70b07e8182f57
xxKeefer <xxkeefer.code@gmail.com>
2022-01-01 11:02:32 +1000

    optimised text components

--- src/components/Common/Text/GBText.tsx
+++ src/components/Common/Text/GBText.tsx
@@ -2,14 +2,9 @@ import styled from 'styled-components'
 
 interface TextProps {
     fontSize: number
-    children: string
 }
 
-export const GBText = ({ fontSize, children }: TextProps) => {
-    const Text = styled.p`
-        font-family: 'PKMN';
-        font-size: ${fontSize}px;
-    `
-
-    return <Text>{children}</Text>
-}
+export const GBText = styled.p<TextProps>`
+    font-family: 'PKMN';
+    font-size: ${(props) => props.fontSize}px;
+`
