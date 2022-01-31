830508fdaa05868500318b40915b3c0a1c83fbf3
xxKeefer <xxkeefer.code@gmail.com>
2022-02-01 00:45:44 +1000

    update title gerator

--- src/utils/getLocationName.ts
+++ src/utils/getLocationName.ts
@@ -4,6 +4,8 @@ export const getLocationName = (pathname: string): string => {
             return 'Pokemon League Utility Tool'
         case '/trainer-card-maker':
             return 'Trainer Card Maker'
+        case '/threat-analysis':
+            return 'Threat Analysis'
         default:
             return ''
     }
